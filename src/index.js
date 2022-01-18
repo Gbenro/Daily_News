6638ab2e8abb60430ed3d13e6be2e0a7245e5060
Gbenro Adesoye <Gbenro_Adesoye@yahoo.com>
2022-01-18 11:13:42 -0600

    redux and routes setup

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import IRoutes from "./routes";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/styles.css";
+
+import { Provider } from "react-redux";
+import ReduxStore from "./store";
+
 ReactDOM.render(
   <React.StrictMode>
-    <IRoutes />
+    <Provider store={ReduxStore()}>
+      <IRoutes />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
