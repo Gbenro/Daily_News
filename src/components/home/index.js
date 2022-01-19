1381129fbcd167e3fcf770e6992dce170a7b895b
Gbenro Adesoye <Gbenro_Adesoye@yahoo.com>
2022-01-19 06:42:54 -0600

    fetching home post pt1

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import HomePosts from "./homePosts";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <HomePosts />
+    </>
+  );
 };
 
 export default Home;
